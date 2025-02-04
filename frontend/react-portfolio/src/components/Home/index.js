43ba00c124f0171d729b4a64a9528f4bb5e5f634
Theodore Chan <theodorechan@mac.trinity.local>
2025-02-03 20:36:05 -0600

    commit

--- frontend/react-portfolio/src/components/Home/index.js
+++ frontend/react-portfolio/src/components/Home/index.js
@@ -3,6 +3,7 @@ import './index.scss';
 import { useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Loader from 'react-loaders';
+import profilePic from '../../assets/images/profile.jpg';
 
 const Home = () => {
     const [letterClass] = useState('text-animate')
@@ -47,33 +48,36 @@ const Home = () => {
 
     return (
         <>
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
-                    <br />
-                    <span className={`${letterClass} _12`}>I'</span>
-                    <span className={`${letterClass} _12`}>m</span>
-                    <AnimatedLetters  
-                        letterClass={letterClass}
-                        strArray={nameArray}
-                        idx={15}/>
-                    <br />
-                    <AnimatedLetters  
-                        letterClass={letterClass}
-                        strArray={jobArray}
-                        idx={16}
-                    />
-                </h1>
-                <h2>Frontend Developer / JavaScript Expert</h2>
-                <Link to="/contact" className='flat-button'>
-                    CONTACT ME
-                </Link>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _12`}>I'</span>
+                        <span className={`${letterClass} _12`}>m</span>
+                        <AnimatedLetters  
+                            letterClass={letterClass}
+                            strArray={nameArray}
+                            idx={15}/>
+                        <br />
+                        <AnimatedLetters  
+                            letterClass={letterClass}
+                            strArray={jobArray}
+                            idx={16}
+                        />
+                    </h1>
+                    <h2>Frontend Developer / JavaScript Expert</h2>
+                    <Link to="/contact" className='flat-button'>
+                        CONTACT ME
+                    </Link>
+                </div>
+                {/* Image Container */}
+                <div className="image-container">
+                    <img src={profilePic} alt="Profile" className="profile-image" />
+                </div>
             </div>
-            {/*<Logo />*/}
-        </div>
-        <Loader type="pacman" />
+            <Loader type="pacman" />
         </>
     );
 }
